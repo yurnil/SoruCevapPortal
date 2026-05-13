0bede62f2eb970cf4e39b965224a7dfd43ce5770
yurnil <niluferfbyurdakul@gmail.com>
2026-05-13 15:09:13 +0300

    admin panel

---
+++ SoruCevapPortal.MVC/Controllers/AdminController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SoruCevapPortal.MVC.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Categories()
+        {
+            return View();
+        }
+    }
+
+}
